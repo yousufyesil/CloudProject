92194d3a398484f2c967a3369a0439739a20b3d1
Yousuf Yesil <yousufyesil@icloud.com>
2024-12-19 09:00:19 +0100

    WSGI

--- bottletube.py
+++ bottletube.py
@@ -7,19 +7,22 @@ import psycopg2
 import requests
 from botocore.exceptions import ClientError
 import json
-from bottle import route, run, template, request, app, static_file
+from bottle import route, run, template, request, app, static_file, default_app
 from boto3 import resource, session
-
 import boto3
+
 
 @route('/hello')
 @route('/healthcheck')
 def healthcheck():
     return 'I feel lucky'
 
+
 @route('/bonusql')
 def bonusql():
     return template('bonusql.tpl', name='Bonusql')
+
+
 @route('/home')
 @route('/')
 def home():
@@ -33,7 +36,6 @@ def home():
             'filename': record[1],
             'category': record[2]
         })
-    # SQL Query goes here later, now dummy data only
 
     return template('home.tpl', name='BoTube Home', items=items)
 
@@ -41,9 +43,12 @@ def home():
 @route('/upload', method='GET')
 def do_upload_get():
     return template('upload.tpl', name='Upload Image')
+
+
 @route('/static/<filename>')
 def serve_static(filename):
     return static_file(filename, root='./static')
+
 
 @route('/upload', method='POST')
 def do_upload_post():
@@ -59,7 +64,7 @@ def do_upload_post():
 
     try:
         name, ext = os.path.splitext(upload.filename)
-        if ext not in ('.png', '.jpg', '.jpeg','.webp','.pdf'):
+        if ext not in ('.png', '.jpg', '.jpeg', '.webp', '.pdf'):
             error_messages.append('File Type not allowed.')
     except:
         error_messages.append('Unknown error.')
@@ -67,27 +72,28 @@ def do_upload_post():
     if error_messages:
         return template('upload.tpl', name='Upload Image', error_messages=error_messages)
 
-    # Save to /tmp folder
+    # Save to images folder
     upload.filename = name + '_' + time.strftime("%Y%m%d-%H%M%S") + ext
     upload.save('images')
 
     # Upload to S3
     data = open('images/' + upload.filename, 'rb')
     s3_resource.Bucket("yesil-20237852").put_object(Key='user_uploads/' + upload.filename,
-                                                                             Body=data,
-                                                                             ACL='public-read')
+                                                    Body=data,
+                                                    ACL='public-read')
 
     # Write to DB
-    cursor.execute(f"INSERT INTO image_uploads (url, category) VALUES ('user_uploads/{upload.filename}', '{category}');")
+    cursor.execute(
+        f"INSERT INTO image_uploads (url, category) VALUES ('user_uploads/{upload.filename}', '{category}');")
     connection.commit()
-    # some code has to go here later
 
-    # Return template
     return template('upload_success.tpl', name='Upload Image')
 
 
-if __name__ == '__main__':
-    # Connect to DB
+# Database and AWS connection setup
+def setup_connections():
+    global connection, cursor, s3_resource
+
     # AWS Secrets Manager Setup
     sm_session = session.Session()
     client = sm_session.client(
@@ -95,13 +101,13 @@ if __name__ == '__main__':
         region_name='us-east-1'
     )
 
-    # Secrets aus dem AWS Secrets Manager abrufen
-    # Der SecretId 'PostgresBottle' enthält die Datenbank-Zugangsdaten
+    # Get secrets from AWS Secrets Manager
     secret = json.loads(
         client.get_secret_value(SecretId='ddaypaper')
         .get('SecretString')
     )
 
+    # Database connection
     connection = psycopg2.connect(
         user=secret['username'],
         host=secret['host'],
@@ -114,10 +120,18 @@ if __name__ == '__main__':
     cursor.execute("SET SCHEMA 'bottletube';")
     connection.commit()
 
-    # some code has to go here
-    # Connect to S3
+    # S3 connection
     s3_resource = resource('s3', region_name='us-east-1')
 
-    # run(host='your_public_dns_name',
+
+# Initialize connections
+setup_connections()
+
+if __name__ == '__main__':
+    # Development server
     run(host='ec2-34-204-125-209.compute-1.amazonaws.com',
         port=5050)
+else:
+    # Production WSGI server
+    os.chdir(os.path.dirname(__file__))
+    application = default_app()
